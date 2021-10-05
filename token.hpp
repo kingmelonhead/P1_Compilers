f55bf2fd78e3c6bf330d1f351075fb2a5daf9c76
John Hackstadt <jhackstadt50@gmail.com>
2021-10-04 23:25:37 -0500

    hard coded the rest of the FSA Table

--- token.hpp
+++ token.hpp
@@ -6,7 +6,11 @@ enum tokenID {ID_Token, Num_Token, Ass_Token, Comp_Token, Greater_Token, Less_To
 std::string tokenNames[] = {"Identifier", "Number", "Assignment Op (=)", "Comparison Op (==)", "Greater Than", "Less Than", "Colon", "Define Op (:=)", "Add", "Subtract", "Multiply", 
                             "Divide", "Percent", "Period", "Opening Parenthesis", "Closing Parenthesis", "Comma", "Opening Curley Brace", "Closing Curly Brace", "Semi-colon",
                             "Opening Square Brace", "Closing Square Brace", "Keyword"};
-                            
+
+//if something is determined to be an identifier token, it will be checked to see if it matches any of the reserved keywords
+//if the ID_Token does end up being a keyword then it gets marked as a Key_Token before being returned
+std::string keywords[] = {"start", "stop", "loop", "while", "for", "label", "exit", "listen", "talk", "program", "if", "then", "assign", "declare", "jump", "else"};   
+            
 struct token { 
     tokenID token_type;
     std::string token_string; 
